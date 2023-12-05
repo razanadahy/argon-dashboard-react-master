85837273295b58e20b6d4cd76cf8fc7cb30167aa
razanadahy <andrianiavo.vit@gmail.com>
2023-12-05 14:23:34 +0300

    dernier commit

--- src/Config.ts
+++ src/Config.ts
@@ -1,10 +1,12 @@
 export function BaseUrlMain(url: string): string {
     // return 'http://10.140.31.21:8080/main/'+url
-     return 'http://localhost:8080/main/'+url
+    //  return 'http://localhost:8080/main/'+url
+     return 'https://followup-14li.onrender.com/'+url
 }
 export function UrlBase(url: string): string{
     // return 'http://10.140.31.21:8080/'+url
-     return 'http://localhost:8080/'+url
+    //  return 'http://localhost:8080/'+url
+     return 'https://followup-14li.onrender.com/'+url
 }
 export function GetMdp() {
     return 'niavo jr'
